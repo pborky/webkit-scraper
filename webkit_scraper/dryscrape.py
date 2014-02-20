d4da43cde1e4367dd9d628aa5b3bfe8abdb8fe57
Peter Boraros <pborky@pborky.sk>
2014-02-20 01:16:31 +0100

    dryscrape integration

---
+++ webkit_scraper/dryscrape.py
@@ -0,0 +1,42 @@
+import dryscrape.mixins
+
+class RemoteMixin(object):
+    def __init__(self, remote):
+        self._remote = remote
+    def __getattr__(self, attr):
+        try:
+            remote = object.__getattribute__(self, '_remote')
+            if hasattr(remote, 'exposed_'+attr):
+                return getattr(remote, attr)
+        except AttributeError: pass
+        raise AttributeError('Not found: %s '% attr)
+    def __setattr__(self, attr, value):
+        try:
+            object.__getattribute__(self, attr)
+            remote = object.__getattribute__(self, '_remote')
+            if hasattr(remote, 'exposed_'+attr):
+                return setattr(remote, attr, value)
+        except AttributeError: pass
+        return object.__setattr__(self, attr, value)
+
+class Node( RemoteMixin,
+            dryscrape.mixins.SelectionMixin,
+            dryscrape.mixins.AttributeMixin):
+    """ Node implementation wrapping a ``webkit_server`` node. """
+    def xpath(self, xpath):
+        return [ Node(n) for n in self._remote.xpath(xpath) ]
+
+
+
+class Driver( RemoteMixin,
+              dryscrape.mixins.WaitMixin,
+              dryscrape.mixins.HtmlParsingMixin):
+    """ Driver implementation wrapping a ``webkit_server`` driver.
+
+    Keyword arguments are passed through to the underlying ``webkit_server.Client``
+    constructor. By default, `node_factory_class` is set to use the dryscrape
+    node implementation. """
+    def __init__(self, remote):
+        super(Driver, self).__init__(remote)
+    def xpath(self, xpath):
+        return [ Node(n) for n in self._remote.xpath(xpath) ]
